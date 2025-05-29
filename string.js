cfce7d3a338d098307163e9f5430ae6eb02d37bb
Kabir058 <riduanulkabir01@gmail.com>
2025-05-29 22:16:39 +0600

    string things( basic)

--- string.js
+++ string.js
@@ -3,3 +3,11 @@ const Division ="Chattogram"
 const District =`Chittagong`
 const Thana = new String("Lohagara")
 console.log( Thana)
+
+const array= [12,34,234,23,53,64]
+console.log(array.length)
+console.log(array[3])
+console.log(District.length)
+console.log(Thana[3])
+array[2]=73;
+console.log(array)

47cb2016bd6d7fee09e9b6c9ea986970d359142a
Kabir058 <riduanulkabir01@gmail.com>
2025-05-29 22:36:16 +0600

    part 3 in trim lower case uppercase compsrison

---
+++ string2.js
@@ -0,0 +1,20 @@
+let varsity = "Bangladesh Army International University of Science and technology."
+console.log(varsity)
+console.log(varsity.toLowerCase())
+console.log(varsity.toUpperCase())
+let subject = 'CHemistry';
+let book = 'chemistry';
+if (book.toLowerCase() === subject.toLowerCase()) {
+    console.log("I am reading chemistry")
+}
+else {
+    console.log("I am reading Math")
+}
+
+let ielts = " Bangla"
+let speak = "Bangla"
+if(ielts.trim()=== speak.trim())
+{console.log( "good")}
+else{
+    console.log("fine")
+}

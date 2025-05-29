df609c987092ee53c47e58e57141d6eb97f93649
Kabir058 <riduanulkabir01@gmail.com>
2025-05-30 00:33:40 +0600

    uses of concat, includes

---
+++ concat.js
@@ -0,0 +1,4 @@
+let name2 =" Neela loves"
+let name1 = " kabir"
+console.log(name2.concat(name1))
+console.log(name2.includes('l'))
--- slice.js
+++ slice.js
@@ -2,4 +2,4 @@ let part = "I was a good boy"
 let part2= "I fall in love"
 let part3 ="Now I'm in dark"
 console.log(part3.slice(1,5))
-console.log(part2.split(" "))
+console.log(part2.split("I"))

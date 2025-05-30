6fa4d85881cb8fa08c1f0cb22928d1d46cee3a78
Kabir058 <riduanulkabir01@gmail.com>
2025-05-30 12:00:29 +0600

    2nd phase of reverse third technique applied

--- reverse.js
+++ reverse.js
@@ -1,9 +1,22 @@
-let str= "A quick brown fox jumps"
-rev = " "
-for(let letter of str)
-{   
-    rev = letter + rev;
+// let str= "A quick brown fox jumps"
+// rev = " "
+// for(let letter of str)
+// {   
+//     rev = letter + rev;
     
 
-}
-console.log(rev)
+// }
+// console.log(rev)
+
+// let sentence = ' I am learning future '
+// let rev = " "
+// for(i=0; i<sentence.length; i++)
+// {    let letter = sentence[i];
+//     rev= letter + rev;
+// }
+// console.log(rev)
+
+let sentence  = " I love cooking food"
+let reversed = sentence.split('').reverse().join('');
+
+console.log(reversed)

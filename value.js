30f287f3d80e9a91df4ad3ea094e5ed1b7a28c93
Kabir058 <riduanulkabir01@gmail.com>
2025-05-30 19:28:44 +0600

    value changes using object

---
+++ value.js
@@ -0,0 +1,14 @@
+let person = {
+    name: 'kabir',
+    age: 25,
+    place: 'chawkbazar',
+    profession : ['instructor','developer', 'communicator']
+
+}
+
+person.age =26;
+ person.place=" coxsbazar";
+ person['profession']= ["teacher, devops, engineer"] 
+console.log(person)
+console.log(person.age)
+console.log(person['place'])

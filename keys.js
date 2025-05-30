dbc8dceba5adfff19a8a04f480b2103f57a16bb7
Kabir058 <riduanulkabir01@gmail.com>
2025-05-30 19:43:09 +0600

    uses of object keys, values to check elements in object

---
+++ keys.js
@@ -0,0 +1,12 @@
+let computer ={
+    brand : "DELL",
+    price : "52000",
+    processor : "intel",
+    ram : "8gb",
+}
+
+ let value = Object.keys(computer);
+ console.log(value)
+ let value1 = Object.values(computer);
+ console.log(value1)
+ console.log(computer)

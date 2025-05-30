2e9aea1af70e5d3db164b9997285624fc5221d09
Kabir058 <riduanulkabir01@gmail.com>
2025-05-30 16:48:43 +0600

    object tasks

---
+++ object.js
@@ -0,0 +1,19 @@
+// let bottle = "mum";
+// let bPrice = " 30"
+// let bMesure = 43;
+// let bColour = " white"
+// console.log(bottle+ bPrice+ bColour)
+
+let bottle ={
+    brand : "Kinley",
+    color : "White",
+    Price : "30",
+    isClean: true,
+    exams:
+    {
+        name: "English",
+        marks: 100
+    }
+
+}
+console.log(bottle)

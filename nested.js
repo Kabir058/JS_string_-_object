80e2f24b519ee673690f69eb886a7197127b2265
Kabir058 <riduanulkabir01@gmail.com>
2025-05-30 20:01:05 +0600

    nested object

---
+++ nested.js
@@ -0,0 +1,16 @@
+let college ={
+    name: "BAF Shaheen",
+    class:[11,12],
+    events: "Rag day, 21st feb, independence day",
+    unique:{
+        color: "blue",
+        badge: "Eagle",
+        campus:
+        {
+            area: ['chittagong','Dhaka','Jassore']
+        }
+    }
+
+    
+}
+console.log(college.class[0]+college.unique.campus.area[0])
